8199bb704eb91eb9076c8fbde4f0a2ac69c6e790
gabyballester <gaby.ballester@gmail.com>
2020-08-14 18:55:57 +0200

    Creo arquivo requests para almacenar los endpoints y la api key

---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY = "6944b42404ae9b9ce5b0c403ec0cd6a7";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=es-es`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=es-es`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default requests;
