94ea3d4028975c2fb69fad9bcbdfcba35e8cd12d
gabyballester <gaby.ballester@gmail.com>
2020-08-14 22:20:17 +0200

    Mostrando películas con cierto formato agregado

--- src/Row.jsx
+++ src/Row.jsx
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
+import "./Row.css"
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+
 function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
 
@@ -10,22 +14,32 @@ function Row({ title, fetchUrl }) {
     //Esto se ejecutará cada vez que el contenido de [] cambie
     async function fetchData() {
       const request = await axios.get(fetchUrl);
-      console.log('consoleo results');
+      console.log("consoleo results");
       console.log(request.data.results);
-      setMovies(request.data.results)
+      setMovies(request.data.results);
       return request;
     }
     fetchData();
     //Aquí incluyo variables que provienen de fuera del componente
   }, [fetchUrl]);
-  console.log('consoleo movies');
-  console.log(movies);
+  console.log("consoleo movies");
+  console.table(movies);
 
   return (
-    <div>
+    <div className="row">
       <h2>{title}</h2>
 
-      {/* container -> posters */}
+      <div className="row__posters">
+        {/* Recorro el array de varios row_poster */}
+        {movies.map((movie) => (
+          <img
+            className="row__poster"
+            key={movie.id}
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
     </div>
   );
 }
