65d249a51b901749d747c071642444f7fa9333d8
gabyballester <gaby.ballester@gmail.com>
2020-08-15 14:28:18 +0200

    Maquetado el banner a falta de los estilos

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import requests from "./requests";
+import './Banner';
 
 function Banner() {
   // array vacío por ahora
@@ -22,12 +23,28 @@ function Banner() {
   console.log(movie);
 
   return (
-    <header>
-      {" "}
-      {/* imagen de fondo para header */}
-      {/* title */}
-      {/* Div con 2 botones */}
-      {/* Descripción */}
+    <header
+      className="banner"
+      style={{
+        // backgroundSize: "cover",
+        backgroundImage: `url(
+        "http://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+        // backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner__contents">
+        <h1>
+          {/* Muestra el título disponible en este orden de prioridad */}
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+
+        <div className="banner__buttons">
+          <button className="banner__button">Reproducir</button>
+          <button className="banner__button">Mi Lista</button>
+        </div>
+
+        <div className="banner__description">{movie?.overview}</div>
+      </div>
     </header>
   );
 }
